c677ae71d6e5201ecda1848fd5e2413a71135087
Ogada Moses <ogadamoses59@gmail.com>
2022-09-20 08:37:11 +0300

    Initialize Repository

---
+++ index.js
@@ -0,0 +1,10 @@
+// Reverse a string
+function reverse(string) {
+    return Array.from(string).reverse().join("");
+}
+
+// Return true for a palindrome, false otherwise
+function palindrome(string) {
+    let processedContent = string.toLowerCase();
+    return processedContent === reverse(processedContent);
+}
